3e82975081204c2c3db1aef05e73d54301e63296
Duong Hien Ngo <48897477+ngohienduong@users.noreply.github.com>
2021-07-11 11:15:00 +0700

    fix bug

--- utils/common_config.py
+++ utils/common_config.py
@@ -217,13 +217,13 @@ def get_train_transformations(p):
     elif p['augmentation_strategy'] == 'simclr_base':
         # Augmentation strategy from the SimCLR paper\
         return transforms.Compose([
-            transforms.RandomChoice(
+            transforms.RandomChoice([
                 transforms.RandomResizedCrop(**p['augmentation_kwargs']['random_resized_crop']),
                 transforms.RandomHorizontalFlip(),
                 transforms.RandomApply([
                     transforms.ColorJitter(**p['augmentation_kwargs']['color_jitter'])
                 ], p=p['augmentation_kwargs']['color_jitter_random_apply']['p']),
-                transforms.RandomGrayscale(**p['augmentation_kwargs']['random_grayscale'])
+                transforms.RandomGrayscale(**p['augmentation_kwargs']['random_grayscale'])]
                 ),
             transforms.ToTensor(),
             transforms.Normalize(**p['augmentation_kwargs']['normalize'])
@@ -232,13 +232,13 @@ def get_train_transformations(p):
     elif p['augmentation_strategy'] == 'simclr_cont':
         # Augmentation strategy from the SimCLR paper\
         return transforms.Compose([
-            transforms.RandomChoice(
+            transforms.RandomChoice([
                 transforms.RandomResizedCrop(**p['augmentation_kwargs']['random_resized_crop']),
                 transforms.RandomHorizontalFlip(),
                 transforms.RandomApply([
                     transforms.ColorJitter(**p['augmentation_kwargs']['color_jitter'])
                 ], p=p['augmentation_kwargs']['color_jitter_random_apply']['p']),
-                transforms.RandomGrayscale(**p['augmentation_kwargs']['random_grayscale'])
+                transforms.RandomGrayscale(**p['augmentation_kwargs']['random_grayscale'])]
                 ),
             transforms.ToTensor(),
             transforms.Normalize(**p['augmentation_kwargs']['normalize'])
